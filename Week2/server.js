7bacb035b9990d01392faea9c4527fb5046bfe21
unknown <minhthac1511@gmail.com>
2025-11-18 19:31:52 +1100

    week2

--- Week2/server.js
+++ Week2/server.js
@@ -7,20 +7,17 @@ app.use(express.json());
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
 // In-memory array to store quotes
-let quotes = [
-  "The best way to predict the future is to invent it.",
-  "Life is 10% what happens to us and 90% how we react to it.",
-  "The only limit to our realization of tomorrow is our doubts of today.",
-  "Do not wait to strike till the iron is hot; but make it hot by striking.",
-];
 
 // GET endpoint to retrieve a random quote
-// Usage example: http://localhost:3000/api/quote
-app.get("/api/quote", (req, res) => {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  res.json({ quote: quotes[randomIndex] });
-});
-// Optional: POST endpoint to add a new quote
+// Usage example: http://localhost:3000/add
+
+app.get ("/add", (req,res) => {
+    const num1 = parseFloat(req.query.number1);
+    const num2 = parseFloat(req.query.number2);
+
+    const total = num1 + num2;
+res.send(`The total is ${total}`);
+})
 
 app.listen(3000, () => {
   console.log(`Server is listening on ${port}`);
